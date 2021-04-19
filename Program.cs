69edac21ac4e291fcfc15539a7654ca12cce08c3
oleksii borodai <oleksa.borodie@gmail.com>
2021-04-19 19:40:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace funcOverriden
+{
+	class Program
+	{
+		static IntPtr GetHandle()
+		{
+			return IntPtr.Zero;
+		}
+
+		static void Main(string[] args)
+		{
+			WithParentActivityOrWindow(GetHandle);
+		}
+		static void WithParentActivityOrWindow(Func<object> parentActivityOrWindowFunc) { }
+		static void WithParentActivityOrWindow(Func<IWin32Window> windowFunc) { }
+		static void WithParentActivityOrWindow(Func<IntPtr> windowFunc) { }
+	}
+}
